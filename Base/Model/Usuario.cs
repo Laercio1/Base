0bf5144d74648d115c27812212e7a2d412b12ad0
Laércio Fernandes <90804219+Laercio1@users.noreply.github.com>
2022-05-06 21:36:51 -0300

    Inicio do controle de usuário
    Foram implementadas as classe Usuário, Permissão, GrupoUsuario.
    Foi dado inicio na implementação da classe UsuarioBLL.
    
    Missão Para a próxima aula, terminar a classe UsuarioBLL e fazer o CRUD no banco de dados.

---
+++ Base/Model/Usuario.cs
@@ -0,0 +1,33 @@
+﻿namespace Model
+{
+    public class Usuario
+    {
+        private string nomeUsuario;
+        private int id;
+        private bool ativo;
+        private string senha;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string NomeUsuario
+        {
+            get { return nomeUsuario; }
+            set { nomeUsuario = value; }
+        }
+        public string Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+        public bool Ativo
+        {
+            get { return ativo; }
+            set { ativo = value; }
+        }
+
+
+    }
+}
