16ebae2c5474832a298455a18141713e658281ad
Laércio Fernandes <90804219+Laercio1@users.noreply.github.com>
2022-05-18 21:55:07 -0300

    Foi implementado o método de excluir, método de buscar. e iniciado o método de alterar.
    Foi criado o método de excluir, buscar, e iniciado o método de alterar.

--- Base/DAL/UsuarioDAL.cs
+++ Base/DAL/UsuarioDAL.cs
@@ -50,7 +50,7 @@ namespace DAL
                 _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
 
                 return _usuario;
-                
+
             }
             catch (SqlException ex)
             {
@@ -65,7 +65,98 @@ namespace DAL
                 cn.Close();
             }
         }
+        public DataTable Buscar(string _filtro)
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = @"User ID=SA;Initial Catalog=Loja;Data Source=.\SQLEXPRESS2019;Password=Senailab05";
+                SqlCommand cmd = new SqlCommand();
+                da.SelectCommand = cmd;
+                da.SelectCommand.Connection = cn;
+                da.SelectCommand.CommandText = "SP_BuscarUsuario";
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                SqlParameter pfiltro = new SqlParameter();
+                pfiltro.Value = _filtro;
+                da.Fill(dt);
+                return dt;
 
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+        public void Excluir(int _id)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = @"User ID=SA;Initial Catalog=Loja;Data Source=.\SQLEXPRESS2019;Password=Senailab05"; ;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_ExcluirUsuario";
+                SqlParameter pid = new SqlParameter();
+                pid.Value = _id;
+                cmd.Parameters.Add(pid);
+
+                cn.Open();
+                int resultado = cmd.ExecuteNonQuery();
+                if (resultado != 1)
+                    throw new Exception("Não possível excluir o usuário: " + _id.ToString());
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Usuario Alterar(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = @"User ID = SA; Initial Catalog = Loja; Data Source =.\SQLEXPRESS2019; Password = Senailab05";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_AlterarUsuario";
+
+                return _usuario;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
 
